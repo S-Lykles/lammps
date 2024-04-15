616eb5dfbaaaf3fadd514007d7e2345802003c00
Sieds Lykles <93992551+S-Lykles@users.noreply.github.com>
2024-04-15 17:50:34 +0200

    split run into equilibrium and nvt

--- my_tests/Simulation Data Conductivities/NaCl new/run.sh
+++ my_tests/Simulation Data Conductivities/NaCl new/run.sh
@@ -3,16 +3,16 @@
 #SBATCH --job-name="lmp_test_nonaveraged"
 #SBATCH --account=education-ae-msc-ae
 #SBATCH --partition=compute
-#SBATCH --time=01:00:00
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=12
-#SBATCH --mem-per-cpu=2G
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=12
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=1G
 #SBATCH --nodes=1
 
 module load 2023r1
-module load intel/oneapi-all
+# module load intel/oneapi-all
 
 export I_MPI_PMI_LIBRARY=/cm/shared/apps/slurm/current/lib64/libpmi2.so
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-srun ~/lammps/build/lmp -in ./in.simulation
+srun ~/lammps/build/lmp -sf omp -in ./in.equilibrium
